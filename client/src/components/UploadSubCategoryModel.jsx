69e665f2409134ebf01c18cd9cc689ad326e0db7
krishnakant rajput <krishnakant.23scse1040201@galgotiasuniversity.ac.in>
2025-05-30 17:32:34 +0530

    Update UploadSubCategoryModel.jsx

--- client/src/components/UploadSubCategoryModel.jsx
+++ client/src/components/UploadSubCategoryModel.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { IoClose } from "react-icons/io5";
-import uploadImage from '../utils/UploadImage';
+import uploadImage from '../utils/Uploadimage';
 import { useSelector } from 'react-redux';
 import Axios from '../utils/Axios';
 import SummaryApi from '../common/SummaryApi';
